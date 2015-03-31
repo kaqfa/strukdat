247c91012c59b478f791ada89e2af4034771eb8a
Fahri Firdausillah <elfaatta@gmail.com>
2015-03-31 21:58:23 +0700

    queue versi valid

--- latihan/IntellijIdea/queue/src/fahri/lat/QueueApp.java
+++ latihan/IntellijIdea/queue/src/fahri/lat/QueueApp.java
@@ -1,52 +1,55 @@
-package fahri.lat.queue;
+package fahri.lat;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author kaqfa
+ */
 public class QueueApp {
-
-    public static Queue1 q1 = new Queue1();
-    public static Queue2 q2 = new Queue2();
-    public static Queue3 q3 = new Queue3();
-
+    
     public static void performTest(int jmlData){
         int qSize = jmlData/10;
         int inputRound = qSize/2;
         int dataIn = 0;
-        q2.createEmpty(qSize);
+        Queue q = new Queue3();
+        q.createEmpty(jmlData);
 
         System.out.println("uji coba dimulai");
 
         // isi hingga queue full
         while(dataIn < qSize){
-            q2.enqueue((int)(Math.random()*100));
+            q.add((int)(Math.random()*100));
             dataIn++;
-            System.out.println("data ke "+dataIn);
+//            System.out.println("data ke "+dataIn);
         }
         while(dataIn < jmlData){
             int delCount = 0;
             // hapus setengah
             while (delCount < inputRound){
-                q2.dequeue();
+                q.remove();
                 delCount++;
             }
             // tambah lagi setengah
             while (delCount > 0){
-                q2.enqueue((int)(Math.random()*100));
+                q.add((int)(Math.random()*100));
                 dataIn++;
-                System.out.println("data ke "+dataIn);
+//                System.out.println("data ke "+dataIn);
                 delCount--;
             }
         }
         // terakhir masih ada setengah data, dihapus sekalian
         for(int delCount = 0;delCount < inputRound; delCount++){
-            q2.dequeue();
+            q.remove();
         }
 
         System.out.println("selesai menguji coba sebanyak "+jmlData+" data");
     }
 
     public static void main(String[] args) {
-
-        int[] jmlData = {10}; //, 10000, 100000, 1000000
-
-        performTest(100);
+        performTest(100000000);
     }
+    
 }
